4007deb8ea2bcf21c697b60e220d1f3459d6b81d
jaewan <jw.ryu@plingo.io>
2019-07-13 15:22:56 +0900

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>dwaiadwjdawiojo</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+
+export default App;
