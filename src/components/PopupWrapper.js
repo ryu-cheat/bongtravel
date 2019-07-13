d38061f3cd6b75f39761c2271a6050f065178488
jaewan <jw.ryu@plingo.io>
2019-07-13 15:24:41 +0900

    폴더 구조잡기

---
+++ src/components/PopupWrapper.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+
+export default App;
