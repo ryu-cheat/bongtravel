a0f3937b67892833f6a5dee52782bd935a565960
jaewan <jw.ryu@plingo.io>
2019-07-28 18:54:24 +0900

    redux걷어내고 react-lingost(자체개발) 사용

---
+++ src/lingost/api.js
@@ -0,0 +1,5 @@
+const lingost = require('./')
+
+export const loginCheck = () => {
+     
+}
---
+++ src/lingost/index.js
@@ -0,0 +1,7 @@
+const { createState } = require('react-lingost')
+
+
+export const user = createState({
+     isLogin: false,
+     info: {  }
+})
